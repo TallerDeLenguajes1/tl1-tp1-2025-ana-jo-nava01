462b8228e76ab5ebfaa8ac739e4143d3ff991bee
ana-jo-nava01 <anajonava14@gmail.com>
2026-04-06 23:55:13 -0300

    segundo commit tp1_1.c

--- tp1_1.c
+++ tp1_1.c
@@ -2,7 +2,16 @@
 
 int main() {
 
-    printf("Hola mundo.");
+    printf("Hola mundo.\n");
+
+    int var = 1;
+    int *p = &var;
+
+    printf("Contenido del puntero: %d\n", *p);
+    printf("Direccion de memoria almacenada por el puntero: %p\n", p);
+    printf("Direccion de memoria de la variable: %p\n", &var);
+    printf("Direccion de memoria del puntero: %p\n", &p);
+    printf("Tamaño de memoria utilizado por la variable: %d", sizeof(var));
 
     return 0;
 }
